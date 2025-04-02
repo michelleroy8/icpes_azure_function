5cfae6ab447abd7995d9af35b1a6d5491a744314
Michelle Roy <79749527+michelleroy8@users.noreply.github.com>
2025-04-02 16:28:20 +0530

    Update CreateResourceGroup.cs

--- AzureFunctionApp/CreateResourceGroup.cs
+++ AzureFunctionApp/CreateResourceGroup.cs
@@ -14,7 +14,7 @@ using Microsoft.Rest;
 
 namespace AzureFunctionApp
 {
-    public static class CreateResourceGroup
+    public static class CreateResourceGroup   //this is a function implementation when evoked this creates a resource group, this picks the resouce group name and location from POST API request body
     {
         [FunctionName("CreateResourceGroup")]
         public static async Task<IActionResult> Run(
